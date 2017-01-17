1da17475d2685ce4c5027a668cf79875a1ccfaaf
Robert <robert.khayreev@gmail.com>
2017-01-17 16:02:45 +0500

    delete unnecessary files

--- proj.ios/build/SharedPrecompiledHeaders/template-Prefix-dzutcyywzneqrgajtvngsgwvnyck/template-Prefix.pch.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/robertkhayreev/ios_workspace/SalmonEngineTemplate/proj.ios/template-Prefix.pch \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator10.2.sdk/usr/include/Availability.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator10.2.sdk/usr/include/AvailabilityInternal.h
--- proj.ios/build/salmontemplate.build/Debug-iphonesimulator/salmontemplate.build/Objects-normal/x86_64/CustomGLKView-BF7820DFD49B288C.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/robertkhayreev/ios_workspace/double-hit-balls/proj.ios/CustomGLKView.mm \
-  /Users/robertkhayreev/ios_workspace/double-hit-balls/proj.ios/CustomGLKView.h \
-  ../../engine/include/Utils/IosApi/ObjC/GLKViewTemplate.h
